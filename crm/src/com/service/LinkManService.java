73b1b272b9845d292e7e8df1423654a3800823b7
dengwanghua <348703586@qq.com>
2017-07-10 19:45:53 +0800

    修改联系人+联系人查看

--- crm/src/com/service/LinkManService.java
+++ crm/src/com/service/LinkManService.java
@@ -1,9 +1,15 @@
 package com.service;
 
+import org.hibernate.criterion.DetachedCriteria;
+
 import com.domain.LinkMan;
+import com.utils.PageBean;
 
 public interface LinkManService {
 
 	public void save(LinkMan linkMan);
 
+	public PageBean getPageBean(DetachedCriteria dc, Integer currentPage, Integer pageSize);
+	
+	LinkMan getById(Long lkm_id);
 }
--- crm/src/com/service/impl/LinkManServiceImpl.java
+++ crm/src/com/service/impl/LinkManServiceImpl.java
@@ -1,5 +1,8 @@
 package com.service.impl;
 
+import java.util.List;
+
+import org.hibernate.criterion.DetachedCriteria;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,13 +10,32 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dao.LinkManDao;
 import com.domain.LinkMan;
 import com.service.LinkManService;
+import com.utils.PageBean;
 @Transactional(isolation=Isolation.REPEATABLE_READ,propagation=Propagation.REQUIRED,readOnly=false)
 public class LinkManServiceImpl implements LinkManService {
 	private LinkManDao lmd;
+	
+	@Override
+	public PageBean getPageBean(DetachedCriteria dc, Integer currentPage, Integer pageSize) {
+		//1 调用Dao查询总记录数
+		Integer totalCount = lmd.getTotalCount(dc);
+		//2 创建PageBean对象
+		PageBean pb = new PageBean(currentPage, totalCount, pageSize);
+		//3 调用Dao查询分页列表数据
+		
+		List<LinkMan> list = lmd.getPageList(dc,pb.getStart(),pb.getPageSize());
+		//4 列表数据放入pageBean中.并返回
+		pb.setList(list);
+		return pb;
+	}
 	@Override
 	public void save(LinkMan linkMan) {
-		lmd.save(linkMan);
+		lmd.saveOrUpdate(linkMan);
 
+	}
+	@Override
+	public LinkMan getById(Long lkm_id) {
+		return lmd.getById(lkm_id);
 	}
 	public LinkManDao getLmd() {
 		return lmd;
@@ -21,5 +43,6 @@ public class LinkManServiceImpl implements LinkManService {
 	public void setLmd(LinkManDao lmd) {
 		this.lmd = lmd;
 	}
+	
 	
 }
